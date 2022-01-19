5fc20047001ccca7ffdf068990730d215b569af9
sxeix <sxeixarts@gmail.com>
2022-01-19 21:58:54 +0000

    Introduce average stats

--- src/app/stats/stats.component.ts
+++ src/app/stats/stats.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FlaskService } from '../flask.service';
 
 @Component({
     selector: 'app-stats',
@@ -8,10 +9,41 @@ import { Component, Input, OnInit } from '@angular/core';
 export class StatsComponent implements OnInit {
 
     @Input() user = '';
+
+    userStatistics: Object[];
+
+    averageWpmString = '';
+    averageCpmString = '';
     
-    constructor() { }
+    constructor(private service: FlaskService) { }
 
     ngOnInit(): void {
+        this.getStatistics();
+    }
+
+    getStatistics() {
+        if (this.user !== '') {
+            this.service.getUserStats(this.user).subscribe(response => {
+                console.log(response);
+                this.userStatistics = response['stats'];
+                this.averageStats();
+            })
+        }
+    }
+
+    averageStats() {
+        let averageWpm = 0;
+        let averageCpm = 0;
+        this.userStatistics.forEach(element => {
+            console.log(element['wpm']);
+            averageWpm = averageWpm + element['wpm'];
+            averageCpm = averageCpm + element['cpm'];
+        });
+        averageWpm = averageWpm / this.userStatistics.length;
+        this.averageWpmString =  'Average wpm: ' + Math.round(averageWpm);
+
+        averageCpm = averageCpm / this.userStatistics.length;
+        this.averageCpmString =  'Average cpm: ' + Math.round(averageCpm);
     }
 
 }
